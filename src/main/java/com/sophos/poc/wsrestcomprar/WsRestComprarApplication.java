28fa48292f9cabeef3a69531bd260fb1c846e30d
Jeisson <jeisson@nuevalengua.com>
2019-05-16 08:49:21 -0500

    Linea base

---
+++ src/main/java/com/sophos/poc/wsrestcomprar/WsRestComprarApplication.java
@@ -0,0 +1,13 @@
+package com.sophos.poc.wsrestcomprar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRestComprarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRestComprarApplication.class, args);
+	}
+
+}
